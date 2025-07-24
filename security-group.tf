b5136b6819b6fd515b13d331a18e494ebe6b1122
Timmy Babayeju <oktbabs@gmail.com>
2025-07-24 14:56:15 +0100

    update security-group

--- security-group.tf
+++ security-group.tf
@@ -35,3 +35,26 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_security_group" "teetest-import-private-sg" {
+  name        = "teetest-import-private-sg"
+  description = "Security-group to test-EC2 instances using terraform import"
+  vpc_id      = aws_vpc.teetest-vpc-import.id
+
+  tags = {
+    Name = "teetest-import-private-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh-priv" {
+  security_group_id = aws_security_group.teetest-import-private-sg.id
+  cidr_ipv4         = "10.0.1.0/24"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_priv" {
+  security_group_id = aws_security_group.teetest-import-private-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
